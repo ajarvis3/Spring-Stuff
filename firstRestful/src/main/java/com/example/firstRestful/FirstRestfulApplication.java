62e04ef672680e421511cadc098cd64da89a9a47
Andrew Jarvis <33203802+ajarvis3@users.noreply.github.com>
2021-01-20 13:35:48 -0500

    First Restful Service

---
+++ firstRestful/src/main/java/com/example/firstRestful/FirstRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstRestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestfulApplication.class, args);
+	}
+
+}
---
+++ firstRestful/src/test/java/com/example/firstRestful/FirstRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstRestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
