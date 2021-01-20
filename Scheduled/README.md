27d158360173a5da2e5d5ec834be8891817f70b6
Andrew Jarvis <33203802+ajarvis3@users.noreply.github.com>
2021-01-20 17:56:47 -0500

    Scheduling

---
+++ Scheduled/README.md
@@ -0,0 +1,7 @@
+# Scheduling
+
+Build with
+`gradlew build`
+
+Run the jar file with
+`java -jar build/libs/Scheduled-0.0.1-SNAPSHOT.jar`
---
+++ Scheduled/src/test/java/com/example/Scheduled/ScheduledApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Scheduled;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduledApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
